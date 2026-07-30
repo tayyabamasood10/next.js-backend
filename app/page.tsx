28afce0c8a104f7a7bf6c0b588bcbc1a331d4c73
fafgames4u <tayyabamasood8006@gmail.com>
2026-07-30 23:27:15 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 // import Navbar from "./components/Navbar";
-import Navbar from "./components/Navbar";
+
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
+  
     <div className="container">
       <h1>Home</h1>
       <p>Welcome to our website.</p>
