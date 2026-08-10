0335fe9f29ee5ee00469e5fe3c7c051fc25bc004
fafgames4u <tayyabamasood8006@gmail.com>
2026-08-10 22:10:51 +0500

    updated

--- supabase/migrations/20260804120554_create_profiles_table.sql
+++ supabase/migrations/20260804120554_create_profiles_table.sql
@@ -1,26 +1,3 @@
--- create table profiles (
---   id uuid primary key references auth.users(id) on delete cascade,
---   full_name text,
---   business_name text,
---   email text unique,
---   created_at timestamptz default now()
--- );
-
--- alter table profiles enable row level security;
-
--- create policy "Users can view their own profile"
---   on profiles for select
---   using (auth.uid() = id);
-
--- create policy "Users can insert their own profile"
---   on profiles for insert
---   with check (auth.uid() = id);
-
--- create policy "Users can update their own profile"
---   on profiles for update
---   using (auth.uid() = id);
-
-  
 
   create table public.profiles (
   id uuid primary key references auth.users(id) on delete cascade,
